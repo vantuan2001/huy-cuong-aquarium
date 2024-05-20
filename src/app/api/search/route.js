bf5d5202869014cda66e96b1a9678a6b36024173
vantuan2001 <tavantuanpytn@gmail.com>
2024-05-20 23:44:46 +0700

    Cập nhật lần 18

--- src/app/api/search/route.js
+++ src/app/api/search/route.js
@@ -5,11 +5,9 @@ import { NextResponse } from "next/server";
 export const GET = async (request) => {
   try {
     await connectToDb();
-    const url = new URL(request.nextUrl);
-    const { searchParams } = url;
-    const name = searchParams.get("title");
-    // const titleQuery = searchParams.searchParams.get("title");
-    const regex = new RegExp(name, "i");
+    const { searchParams } = new URL(request.url);
+    const title = searchParams.get("title");
+    const regex = new RegExp(title, "i");
 
     const products = await Product.find({
       title: { $regex: regex },
@@ -18,8 +16,8 @@ export const GET = async (request) => {
     return NextResponse.json(products);
   } catch (err) {
     console.error(err);
-    return NextResponse.error(new Error("Unable to retrieve products!"));
+    return NextResponse.error(new Error("Không thể lấy thông tin sản phẩm!"));
   }
 };
 
-export const dynamic = "auto";
+export const dynamic = "force-dynamic";
